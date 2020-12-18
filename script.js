4040071e86bd70375a19ac4e61f419534f956ec3
NasroDZZ <41264473+NasroDZZ@users.noreply.github.com>
2020-12-19 00:32:20 +0100

    adding starting project

---
+++ script.js
@@ -0,0 +1,21 @@
+class Player {
+	constructor(name,type) {
+		console.log(this);
+		this.name = name;
+		this.type = type;
+	}
+	introduce() {
+		console.log(`Hi i am ${this.name},I'm a ${this.type}`)
+	}
+}
+class Wizard extends Player {
+	constructor(name,type) {
+		super(name,type)
+	}
+	play(){
+		console.log(`WEEEEEE I'm a ${this.type}`);
+	}
+}
+
+const wizard1 = new Wizard('Shelly','Healer');
+const wizard2 = new Wizard('Shawn','Dark Mag
